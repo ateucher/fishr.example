2f85aca3854c404b5a33eeb4208c34b7d9126a4e
Andy Teucher <andy.teucher@gmail.com>
2026-02-23 16:40:37 -0800

    Add tests

--- R/cpue.R
+++ R/cpue.R
@@ -5,6 +5,7 @@
 #' @param catch Numeric vector of catch (e.g., kg)
 #' @param effort Numeric vector of effort (e.g., hours)
 #' @param gear_factor Numeric adjustment for gear standardization (default is 1)
+#' @param verbose Logical indicating whether to print processing messages (default is FALSE)
 #'
 #' @return A numeric vector of CPUE values
 #' @export
@@ -12,7 +13,11 @@
 #' @examples
 #' cpue(100, 10)
 #' cpue(100, 10, gear_factor = 0.5)
-cpue <- function(catch, effort, gear_factor = 1) {
+cpue <- function(catch, effort, gear_factor = 1, verbose = FALSE) {
+  if (verbose) {
+    message("Processing ", length(catch), " records")
+  }
+
   raw_cpue <- catch / effort
 
   raw_cpue * gear_factor
